45d863f9594e3d3a920e95b92134d29ff5167869
mohitb0306 <mohitbhrgv0306@gmail.com>
2026-09-15 02:32:04 +0530

    Add BMRCalculator to Registry component

--- src/components/calculators/Registry.tsx
+++ src/components/calculators/Registry.tsx
@@ -5,6 +5,7 @@ import { FiTool } from "react-icons/fi"; // Import the specific icon used in the
 import SafeIcon from "@/components/common/SafeIcon";
 import EMICalculator from "./EMICalculator";
 import BMICalculator from "./BMICalculator";
+import BMRCalculator from "./BMRCalculator";
 
 // --- PLACEHOLDERS (To be replaced in Phase 1) ---
 // We keep these simple for now, but they will be fully implemented later.
@@ -26,6 +27,10 @@ export const renderCalculatorComponent = (key: string, props: any) => {
     case "bmi":
     case "bmi-calculator":
         return <BMICalculator {...props} />;
+
+    case "bmr":
+    case "bmr-calculator":
+        return <BMRCalculator {...props} />;
 
     case "sip": 
     case "sip-calculator":
